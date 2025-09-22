6052ba616b11d91fd8380d31b2c399f8e878cd26
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-09-23 01:20:32 +0200

    add the yabai indicator cask

---
+++ Casks/yabai-indicator.rb
@@ -0,0 +1,13 @@
+cask "yabai-indicator" do
+  version "0.3.4"
+  sha256 "5dde182daebe3f442193232d3cdefbd66364235c7fa0490bbc5e67f42e1912ef"
+
+  url "https://github.com/xiamaz/YabaiIndicator/releases/download/#{version}/YabaiIndicator-#{version}.zip"
+  name "YabaiIndicator"
+  desc "macOS menubar applet for the yabai tiling window manager"
+  homepage "https://github.com/xiamaz/YabaiIndicator"
+
+  depends_on formula: "yabai"
+
+  app "YabaiIndicator-#{version}/YabaiIndicator.app"
+end
